6104726dbc4864f16c2c0fab6ce399cdf368fce5
Jio Mamaoag <jiomamaoag@gmail.com>
2016-11-03 11:46:56 +0800

    Organized Routes

--- routes/web.php
+++ routes/web.php
@@ -30,19 +30,22 @@ Route::get('/', function () {
   Route::resource('admin/user', 'Admin\UserManagementController',['as'=>'admin']);
   /*  member */
   Route::get('home', 'User\UserController@index')->name('user.index'); //News Feed
-  Route::post('home', 'User\UserController@addPost')->name('user.post');
   Route::get('/{id}','User\UserController@see')->name('user.see');
   Route::get('/{id}/edit','User\UserController@edit')->name('user.edit');
   Route::get('{id}/subscribe','User\UserController@like')->name('user.sub');
-  Route::get('post/{id}','User\UserController@seePost')->name('user.seepost');
-  Route::post('post/{id}/reply','User\UserController@reply')->name('user.reply');
-  Route::get('post/{id}/like','User\UserController@like')->name('user.like');
-  Route::get('post/{id}/dislike','User\UserController@dislike')->name('user.dislike');
   Route::put('{id}/update','User\UserController@update')->name('user.update');
   Route::put('{id}/avatar','User\UserController@updateAvatar')->name('user.update.avatar');
   Route::put('{id}/banner','User\UserController@updateBanner')->name('user.update.banner');
-
+  /* Post and see and like and reply */
+  Route::post('home', 'User\UserController@addPost')->name('user.post');
+  Route::get('post/{id}','User\UserController@seePost')->name('user.seepost');
+  Route::get('post/{id}/dislike','User\UserController@dislike')->name('user.dislike');
+  Route::get('post/{id}/like','User\UserController@like')->name('user.like');
+  Route::post('post/{id}/reply','User\UserController@reply')->name('user.reply');
   /* Settings and stuff */
   Route::get('{id}/settings', 'User\UserController@getAccountSettings')->name('account.settings');
   Route::get('{id}/settings/privacy', 'User\UserController@getAccountSettings')->name('account.privacy');
   Route::get('{id}/settings/change/password', 'User\UserController@getAccountSettings')->name('account.changepass');
+  Route::post('{id}/settings', 'User\UserController@getAccountSettings')->name('account.settings.update');
+  Route::post('{id}/settings/privacy', 'User\UserController@getAccountSettings')->name('account.privacy.update');
+  Route::post('{id}/settings/change/password', 'User\UserController@getAccountSettings')->name('account.changepass.update');
